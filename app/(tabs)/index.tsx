d4107b31a3b1e2b3f2b144c88c45f2635d60bdcd
cbujault <c.bujault@gmail.com>
2024-12-17 15:53:30 +0100

    Message qand compteur fini + image qui défile après vidéo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import { StyleSheet, Platform, View, Text, ScrollView, Dimensions, TouchableOpac
 import { Video } from 'expo-av'; // Importez Video de expo-av
 import { Linking } from 'react-native'; // Importez Linking pour la redirection vers une URL
 import { Appearance } from 'react-native';
+import { useRouter } from 'expo-router'; 
 
-import { HelloWave } from '@/components/HelloWave';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
@@ -13,6 +13,8 @@ export default function HomeScreen() {
   Appearance.setColorScheme('dark');
   // Date du festival (23 mai 2025)
   const festivalDate = new Date('2025-05-23T00:00:00');
+  const router = useRouter(); // Hook pour la navigation
+
 
   // Etat du compte à rebours
   const [timeLeft, setTimeLeft] = useState({
@@ -22,6 +24,7 @@ export default function HomeScreen() {
     seconds: 0,
   });
 
+  const [timeExpired, setTimeExpired] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false); // Nouvel état pour savoir si l'utilisateur a défilé
 
   // Mise à jour du compte à rebours chaque seconde
@@ -32,16 +35,11 @@ export default function HomeScreen() {
 
       if (timeDifference <= 0) {
         clearInterval(interval);
-        setTimeLeft({
-          days: 0,
-          hours: 0,
-          minutes: 0,
-          seconds: 0,
-        });
+        setTimeExpired(true); // Compteur expiré
       } else {
         const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
         setTimeLeft({ days, hours, minutes, seconds });
@@ -57,6 +55,10 @@ export default function HomeScreen() {
   // Fonction pour ouvrir un site web
   const openWebsite = () => {
     Linking.openURL('https://www.instagram.com/baillarock_unitedwefest/'); // Remplacez 'http://xxxxx' par l'URL souhaitée
+  };
+
+  const navigateToExplore = () => {
+    router.push('/(tabs)/explore'); // Redirige vers l'onglet Explore
   };
 
   // Fonction pour gérer le défilement avec des seuils asymétriques
@@ -76,65 +78,81 @@ export default function HomeScreen() {
       }
     };
     
-
-
-  return (
-    <ScrollView contentContainerStyle={styles.scrollContainer} onScroll={handleScroll} scrollEventThrottle={16}>
-      <View style={[styles.imageContainer, { height: screenHeight }]}>
-        {/* Afficher la vidéo ou l'image en fonction de l'état isScrolled */}
-        {isScrolled ? (
-          <Image
-            source={require('@/assets/images/Dragon.png')} // Remplacez par votre image
-            style={styles.backgroundImage}
-            resizeMode="cover"
-          />
-        ) : (
-          <Video
-            source={require('@/assets/videos/videohomepage.mp4')} // Remplacez avec votre propre fichier vidéo
-            style={styles.backgroundVideo}
-            resizeMode="cover" // Recouvre l'ensemble de l'écran
-            shouldPlay // Commence la lecture
-            isLooping // Répète la vidéo en boucle
-            isMuted // Vidéo sans son
-          />
-        )}
-        
-        {/* Texte et compte à rebours au-dessus de la vidéo ou de l'image */}
-        <View style={styles.overlay}>
-          {/* Texte du festival ajouté */}
-          <Text style={styles.TitleText}>BAILLAROCK FESTIVAL</Text>
+  
+    return (
+      <ScrollView contentContainerStyle={styles.scrollContainer} onScroll={handleScroll} scrollEventThrottle={16}>
+        <View style={[styles.imageContainer, { height: screenHeight }]}>
+          {/* Afficher la vidéo ou l'image en fonction de l'état isScrolled */}
+          {isScrolled ? (
+            <Image
+              source={require('@/assets/images/Dragon.png')} // Remplacez par votre image
+              style={styles.backgroundImage}
+              resizeMode="cover"
+            />
+          ) : (
+            <Video
+              source={require('@/assets/videos/videohomepage.mp4')} // Remplacez avec votre propre fichier vidéo
+              style={styles.backgroundVideo}
+              resizeMode="cover" // Recouvre l'ensemble de l'écran
+              shouldPlay // Commence la lecture
+              isLooping // Répète la vidéo en boucle
+              isMuted // Vidéo sans son
+            />
+          )}
           
-          {/* Affichage du compte à rebours */}
-          <View style={styles.countdownContainer}>
-            <View style={styles.countdownRow}>
-              <View style={styles.countdownItem}>
-                <Text style={styles.countdownNumber}>{timeLeft.days}</Text>
-                <Text style={styles.countdownLabel}>Jours</Text>
-              </View>
-              <View style={styles.countdownItem}>
-                <Text style={styles.countdownNumber}>{timeLeft.hours}</Text>
-                <Text style={styles.countdownLabel}>Heures</Text>
-              </View>
-              <View style={styles.countdownItem}>
-                <Text style={styles.countdownNumber}>{timeLeft.minutes}</Text>
-                <Text style={styles.countdownLabel}>Minutes</Text>
-              </View>
-              <View style={styles.countdownItem}>
-                <Text style={styles.countdownNumber}>{timeLeft.seconds}</Text>
-                <Text style={styles.countdownLabel}>Secondes</Text>
-              </View>
-            </View>
+          {/* Texte et compte à rebours au-dessus de la vidéo ou de l'image */}
+          <View style={styles.overlay}>
+            {/* Texte du festival ajouté */}
+            <Text style={styles.TitleText}>BAILLAROCK FESTIVAL</Text>
+            
+            {/* Affichage dynamique */}
+            {timeExpired ? (
+              <>
+                {/* Texte lorsque le temps est écoulé */}
+                <Text style={styles.expiredText}>C'est maintenant !</Text>
+                {/* Bouton Voir la programmation */}
+                <TouchableOpacity style={styles.button} onPress={navigateToExplore}>
+                  <Text style={styles.buttonText}>Voir la programmation</Text>
+                </TouchableOpacity>
+              </>
+            ) : (
+              <>
+                {/* Affichage du compte à rebours */}
+                <View style={styles.countdownContainer}>
+                  <View style={styles.countdownRow}>
+                    <View style={styles.countdownItem}>
+                      <Text style={styles.countdownNumber}>{timeLeft.days}</Text>
+                      <Text style={styles.countdownLabel}>Jours</Text>
+                    </View>
+                    <View style={styles.countdownItem}>
+                      <Text style={styles.countdownNumber}>{timeLeft.hours}</Text>
+                      <Text style={styles.countdownLabel}>Heures</Text>
+                    </View>
+                    <View style={styles.countdownItem}>
+                      <Text style={styles.countdownNumber}>{timeLeft.minutes}</Text>
+                      <Text style={styles.countdownLabel}>Minutes</Text>
+                    </View>
+                    <View style={styles.countdownItem}>
+                      <Text style={styles.countdownNumber}>{timeLeft.seconds}</Text>
+                      <Text style={styles.countdownLabel}>Secondes</Text>
+                    </View>
+                  </View>
+                </View>
+
+                {/* Adresse en italique, au-dessus du bouton */}
+                <Text style={styles.addressText}>Saint Georges Les Baillargeaux</Text>
+    
+                {/* Bouton Acheter les billets */}
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => Linking.openURL('https://www.instagram.com/baillarock_unitedwefest/')}
+                >
+                  <Text style={styles.buttonText}>Achetez vos billets !</Text>
+                </TouchableOpacity>
+              </>
+            )}
           </View>
-          
-          {/* Adresse en italique, au-dessus du bouton */}
-          <Text style={styles.addressText}>Saint Georges Les Baillargeaux</Text>
-
-          {/* Le bouton en bas du compteur */}
-          <TouchableOpacity style={styles.button} onPress={openWebsite}>
-            <Text style={styles.buttonText}>Achetez vos billets !</Text>
-          </TouchableOpacity>
         </View>
-      </View>
 
       {/* Etapes d'instructions */}
       <ThemedView style={styles.stepContainer}>
@@ -285,6 +303,22 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 
+  locationText: {
+    fontSize: 12,
+    color: '#fff',
+    marginVertical: 10, // Espacement au-dessus et en dessous
+    fontStyle: 'italic',
+    textAlign: 'center', // Centrer le texte
+  },
+
+ expiredText: {
+  fontSize: 22,
+  color: '#fff',
+  marginVertical: 10, // Espacement au-dessus et en dessous
+  fontWeight: 'bold',
+  textAlign: 'center', // Centrer le texte
+ },
+
   // Nouveau style pour l'adresse en italique et au-dessus du bouton
   addressText: {
     fontSize: 12, // Taille de police petite
@@ -298,5 +332,7 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
+
+  
 });
 
